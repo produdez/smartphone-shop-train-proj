3314b1706b631748ef3d76a5c0b550426031f3de
Produdez <bigtimegangstar@gmail.com>
2021-07-08 09:23:23 +0700

    Refactor some bad commas

--- db/migrate/20210706050522_create_models.rb
+++ db/migrate/20210706050522_create_models.rb
@@ -5,7 +5,7 @@ class CreateModels < ActiveRecord::Migration[6.1]
       t.text :description
       t.timestamps
       t.belongs_to :operating_system, null: false, foreign_key: true
-      t.belongs_to :brand , null: false, foreign_key: true
+      t.belongs_to :brand, null: false, foreign_key: true
     end
   end
 end
--- db/migrate/20210706064358_create_stores.rb
+++ db/migrate/20210706064358_create_stores.rb
@@ -1,7 +1,7 @@
 class CreateStores < ActiveRecord::Migration[6.1]
   def change
     create_table :stores do |t|
-      t.string :name ,null: false, index: { unique: true }
+      t.string :name, null: false, index: { unique: true }
       t.string :location
       t.belongs_to :user, index: { unique: true }, null: false, foreign_key: true
       t.timestamps
