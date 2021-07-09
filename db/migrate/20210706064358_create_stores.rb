ab1d4b17dbb6ef3a22eac50a23430ef00963b670
Produdez <bigtimegangstar@gmail.com>
2021-07-09 09:29:23 +0700

    Update the User role's relation/structure - Current new approach is splitting the access role (admin/normal user) and the business role (employee/manager) - The current approach is:     - More readable, has more scalability     - Clear separation between application/business     - Reduces one connection that's quite miss-leading (between store and user) - But compared to the old approach:     - It's quite difficult to constrains one manager per store at the DB level for this structure (this was the original idea). Instead, I will enforce this constraint on a higher level (model)     - The 'staff' table could be seen as quite 'redundant', but it's there for the separation between authentication and business relations.

--- db/migrate/20210706064358_create_stores.rb
+++ db/migrate/20210706064358_create_stores.rb
@@ -3,7 +3,6 @@ class CreateStores < ActiveRecord::Migration[6.1]
     create_table :stores do |t|
       t.string :name, null: false, index: { unique: true }
       t.string :location
-      t.belongs_to :user, index: { unique: true }, null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/migrate/20210706064506_create_staffs.rb
+++ db/migrate/20210706064506_create_staffs.rb
@@ -1,7 +1,8 @@
-class CreateEmployees < ActiveRecord::Migration[6.1]
+class CreateStaffs < ActiveRecord::Migration[6.1]
   def change
-    create_table :employees do |t|
+    create_table :staffs do |t|
       t.timestamps
+      t.string :role, default: 'staff' # staff or manager
       t.belongs_to :user, index: { unique: true }, null: false, foreign_key: true
       t.belongs_to :store, null: false, foreign_key: true
     end
