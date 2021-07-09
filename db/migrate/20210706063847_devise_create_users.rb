96d2279789e82419e22d7ddbcccd1f072b5a62d7
Produdez <bigtimegangstar@gmail.com>
2021-07-09 14:40:42 +0700

    Changes default user role to normal user, fix decimal scale, clarification comment

--- db/migrate/20210706063847_devise_create_users.rb
+++ db/migrate/20210706063847_devise_create_users.rb
@@ -7,11 +7,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       t.string :email,              null: false, default: ''
       t.string :encrypted_password, null: false, default: ''
       ## Rememberable
-      t.datetime :remember_created_at
+      t.datetime :remember_created_at, comment: 'Account creation time, different from timestamp where it stores update time'
       t.timestamps null: false
       t.string :name, null: false
       t.string :phone
-      t.string :role, null: false, default: 'admin' # admin or user!
+      t.string :role, null: false, default: 'user' # admin or user!
     end
 
     add_index :users, :email, unique: true
