831ff1bd5882d1f18106a31a7b95b2216d8bcda2
Produdez <bigtimegangstar@gmail.com>
2021-07-12 16:59:05 +0700

    Make format helper reusable

--- app/helpers/phones_helper.rb
+++ app/helpers/phones_helper.rb
@@ -1,2 +1,9 @@
 module PhonesHelper
+  def format_price(price)
+    number_to_currency(price, unit: '$')
+  end
+
+  def format_memory(memory)
+    number_to_human(memory, precision: 2, units: {mili: 'MB', unit: 'GB', thousand: 'TB' })
+  end
 end
