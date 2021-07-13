64ae2db51ccdfa7621e47ad966fca73499e174b0
Produdez <bigtimegangstar@gmail.com>
2021-07-13 15:08:30 +0700

    Very rough layout of create form

--- app/controllers/phones_controller.rb
+++ app/controllers/phones_controller.rb
@@ -2,4 +2,28 @@ class PhonesController < ApplicationController
   def index
     @phones = Phone.all
   end
+
+  def new
+    @models = Model.all
+    @phone = Phone.new
+  end
+
+  def create
+    p = params
+    puts 'Phone: ', p
+    model = Model.find_by(name: p[:model])
+    color = Color.find_by(name: p[:color])
+    condition = helpers.get_condition_value(p[:condition])
+
+    phone = Phone.new(
+      model: model, color: color,
+      
+      memory: p[:memory], condition: condition, 
+      price: p[:price], note: p[:note]
+    )
+
+    #! loop quantity
+
+    puts phone.inspect
+  end
 end
--- app/helpers/phones_helper.rb
+++ app/helpers/phones_helper.rb
@@ -4,6 +4,29 @@ module PhonesHelper
   end
 
   def format_memory(memory)
-    number_to_human(memory, precision: 2, units: {mili: 'MB', unit: 'GB', thousand: 'TB' })
+    number_to_human(memory, precision: 2, units: { mili: 'MB', unit: 'GB', thousand: 'TB' })
   end
+
+  def condition_strs
+    puts Phone::CONDITIONS
+    Phone::CONDITIONS.map { |cond| CONDITION_MAPPING[cond.to_sym] }
+  end
+
+  def get_condition_value(cond_str)
+    CONDITION_MAPPING.each do |value, str|
+      return value if str == cond_str
+    end
+  end
+
+  # NOTE: %w[percent99 like_new old used_once brand_new percent98 decent_new usable].freeze
+  CONDITION_MAPPING = {
+    percent99: '99%',
+    like_new: 'Like New',
+    old: 'Old',
+    used_once: 'Used Once',
+    brand_new: 'Brand New',
+    percent98: '98%',
+    decent_new: 'Decently New',
+    usable: 'Usable'
+  }.freeze
 end
