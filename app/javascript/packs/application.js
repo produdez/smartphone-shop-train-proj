b075065ffa8c86753fedaba0b370694fd3716558
Produdez <bigtimegangstar@gmail.com>
2021-07-15 09:09:04 +0700

    Installed JQUERY

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,12 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-
+require('jquery')
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+// include jQuery in global and window scope (so you can access it globally)
+// in your web browser, when you type $('.div'), it is actually refering to global.$('.div')
+global.$ = global.jQuery = jQuery;
+window.$ = window.jQuery = jQuery;
