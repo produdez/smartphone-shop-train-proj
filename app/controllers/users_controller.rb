49979b466edcc82dffb095d2bec61977b8119dd2
Produdez <bigtimegangstar@gmail.com>
2021-07-19 15:39:26 +0700

    Add user info page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,8 @@ class UsersController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home; end
+
+  def show
+    @user = current_user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
       post :delete_selected
     end
   end
+
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
